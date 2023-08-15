093d4d63ae34b24e069b29ae422196fa67b067f5
Fahad Islam <fifahad007@gmail.com>
2023-08-15 21:39:53 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/app.dart';
+
+void main() {
+  // Run your Flutter app
+  runApp(const TaskManagerApp());
+}
---
+++ lib/ui/utils/assets_utils.dart
@@ -0,0 +1,10 @@
+class AssetsUtils {
+  AssetsUtils._();
+
+  static const String _imagePath = 'assets/images';
+
+ // static const String logoSVG = '$_imagePath/logo.svg';
+ static const String logoSVG = '$_imagePath/img1.svg';
+
+  static const String backgroundSVG = '$_imagePath/background.svg';
+}
